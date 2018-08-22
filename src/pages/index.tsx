aa47042bf50f9e8a4caea30c602294be7cc12b5a
Tim Kuminecz <tkuminecz@gmail.com>
2018-08-22 13:31:40 -0400

    fix project name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 export default class HomePage extends React.Component {
   render () {
     return (
-      <h1>next-typescript-react-redux</h1>
+      <h1>next-typescript-redux-saga</h1>
     )
   }
 }
