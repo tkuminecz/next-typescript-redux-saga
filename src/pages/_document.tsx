8d98714243016b358bb1adee09fe01ebde84c0d5
Tim Kuminecz <tkuminecz@gmail.com>
2019-02-22 14:53:38 -0500

    add material-ui

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,8 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet='utf-8'/>
           <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no'/>
+          <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500'/>
+          <link rel='stylesheet' href='https://fonts.googleapis.com/icon?family=Material+Icons'/>
         </Head>
         <body>
           <Main/>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import Button from '@material-ui/core/Button'
 import Counter from '#/components/Counter'
 import Link from 'next/link'
 
@@ -8,6 +9,7 @@ export default class HomePage extends React.Component {
       <>
         <h1>next-typescript-redux-saga</h1>
         <Counter withControls={false}/>
+        <Button variant='contained'>Say hello</Button>
         <Link href='/counter'><a>Counter page</a></Link>
       </>
     )
